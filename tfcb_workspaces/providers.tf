ec4e65d1ff7991ff0822d51d527a29b583a8b646
ppresto <ppresto@hashicorp.com>
2023-01-17 14:05:53 -0800

    2

--- tfcb_workspaces/providers.tf
+++ tfcb_workspaces/providers.tf
@@ -2,14 +2,3 @@ provider "tfe" {
   hostname = var.tfe_hostname
   token    = var.tfe_token
 }
-
-terraform {
-  required_version = ">= 1.0.5"
-  required_providers {
-    tfe = {
-      source  = "hashicorp/tfe"
-      version = "~>0.36.0"
-    }
-  }
-}
-
